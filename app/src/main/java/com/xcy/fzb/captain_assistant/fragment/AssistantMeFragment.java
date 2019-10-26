81b8a1c03331852c292511fffbcb32e99342abbd
ChengLin <ChengLin12138@163.com>
2019-10-26 13:20:17 +0800

    20191026CL1320

--- app/src/main/java/com/xcy/fzb/captain_assistant/fragment/AssistantMeFragment.java
+++ app/src/main/java/com/xcy/fzb/captain_assistant/fragment/AssistantMeFragment.java
@@ -1,6 +1,7 @@
 package com.xcy.fzb.captain_assistant.fragment;
 
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -318,4 +319,32 @@ public class AssistantMeFragment extends AllFragment implements View.OnClickList
 
         initUserMessage();
     }
+
+    // 2.1 定义用来与外部activity交互，获取到宿主activity
+    private FragmentInteraction listterner;
+
+    // 1 定义了所有activity必须实现的接口方法
+    public interface FragmentInteraction {
+        void process(String str);
+    }
+
+    // 当FRagmen被加载到activity的时候会被回调
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        if(activity instanceof FragmentInteraction) {
+            listterner = (FragmentInteraction) activity; // 2.2 获取到宿主activity并赋值
+        } else{
+            throw new IllegalArgumentException("activity must implements FragmentInteraction");
+        }
+    }
+
+    //把传递进来的activity对象释放掉
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listterner = null;
+    }
+
 }
