1ed4e075f42cd0020c360068a1376a3a98b8ff63
TheblueSuMu <869592978@qq.com>
2019-12-11 21:19:18 +0800

    FZB12.11_21:19

--- app/src/main/java/com/xcy/fzb/all/modle/HomeBean.java
+++ app/src/main/java/com/xcy/fzb/all/modle/HomeBean.java
@@ -5,7 +5,7 @@ public class HomeBean {
     /**
      * code : 1
      * msg : 成功
-     * data : {"tradeNumber":0,"invoiceMoney":"0","applyCount":0,"loseEfficacy":1,"reportNumber":0,"earnestMoneyNumber":0,"surplusMoney":"0","backMoney":"0","receivableMoney":"0","accessingNumber":0,"prodjectCount":1,"isIslandNumber":0,"auditNumber":1,"landingNumber":0}
+     * data : {"accessingNoRead":1,"tradeNumber":51,"applyNoRead":"1","invoiceMoney":"0","applyCount":5,"loseEfficacy":72,"reportNumber":43,"earnestMoneyNumber":4,"surplusMoney":"232.41","backMoney":"1.10","loseNoRead":1,"auditNoRead":"1","receivableMoney":"233.52","tradeNoRead":1,"accessingNumber":8,"reportNoRead":1,"prodjectCount":22,"isIslandNumber":6,"isIslandNoRead":1,"earnestMoneyNoRead":1,"auditNumber":31}
      */
 
     private String code;
@@ -38,23 +38,32 @@ public class HomeBean {
 
     public static class DataBean {
         /**
-         * tradeNumber : 0
+         * accessingNoRead : 1
+         * tradeNumber : 51
+         * applyNoRead : 1
          * invoiceMoney : 0
-         * applyCount : 0
-         * loseEfficacy : 1
-         * reportNumber : 0
-         * earnestMoneyNumber : 0
-         * surplusMoney : 0
-         * backMoney : 0
-         * receivableMoney : 0
-         * accessingNumber : 0
-         * prodjectCount : 1
-         * isIslandNumber : 0
-         * auditNumber : 1
-         * landingNumber : 0
+         * applyCount : 5
+         * loseEfficacy : 72
+         * reportNumber : 43
+         * earnestMoneyNumber : 4
+         * surplusMoney : 232.41
+         * backMoney : 1.10
+         * loseNoRead : 1
+         * auditNoRead : 1
+         * receivableMoney : 233.52
+         * tradeNoRead : 1
+         * accessingNumber : 8
+         * reportNoRead : 1
+         * prodjectCount : 22
+         * isIslandNumber : 6
+         * isIslandNoRead : 1
+         * earnestMoneyNoRead : 1
+         * auditNumber : 31
          */
 
+        private int accessingNoRead;
         private int tradeNumber;
+        private String applyNoRead;
         private String invoiceMoney;
         private int applyCount;
         private int loseEfficacy;
@@ -62,12 +71,25 @@ public class HomeBean {
         private int earnestMoneyNumber;
         private String surplusMoney;
         private String backMoney;
+        private int loseNoRead;
+        private String auditNoRead;
         private String receivableMoney;
+        private int tradeNoRead;
         private int accessingNumber;
+        private int reportNoRead;
         private int prodjectCount;
         private int isIslandNumber;
+        private int isIslandNoRead;
+        private int earnestMoneyNoRead;
         private int auditNumber;
-        private int landingNumber;
+
+        public int getAccessingNoRead() {
+            return accessingNoRead;
+        }
+
+        public void setAccessingNoRead(int accessingNoRead) {
+            this.accessingNoRead = accessingNoRead;
+        }
 
         public int getTradeNumber() {
             return tradeNumber;
@@ -75,6 +97,14 @@ public class HomeBean {
 
         public void setTradeNumber(int tradeNumber) {
             this.tradeNumber = tradeNumber;
+        }
+
+        public String getApplyNoRead() {
+            return applyNoRead;
+        }
+
+        public void setApplyNoRead(String applyNoRead) {
+            this.applyNoRead = applyNoRead;
         }
 
         public String getInvoiceMoney() {
@@ -133,6 +163,22 @@ public class HomeBean {
             this.backMoney = backMoney;
         }
 
+        public int getLoseNoRead() {
+            return loseNoRead;
+        }
+
+        public void setLoseNoRead(int loseNoRead) {
+            this.loseNoRead = loseNoRead;
+        }
+
+        public String getAuditNoRead() {
+            return auditNoRead;
+        }
+
+        public void setAuditNoRead(String auditNoRead) {
+            this.auditNoRead = auditNoRead;
+        }
+
         public String getReceivableMoney() {
             return receivableMoney;
         }
@@ -141,12 +187,28 @@ public class HomeBean {
             this.receivableMoney = receivableMoney;
         }
 
+        public int getTradeNoRead() {
+            return tradeNoRead;
+        }
+
+        public void setTradeNoRead(int tradeNoRead) {
+            this.tradeNoRead = tradeNoRead;
+        }
+
         public int getAccessingNumber() {
             return accessingNumber;
         }
 
         public void setAccessingNumber(int accessingNumber) {
             this.accessingNumber = accessingNumber;
+        }
+
+        public int getReportNoRead() {
+            return reportNoRead;
+        }
+
+        public void setReportNoRead(int reportNoRead) {
+            this.reportNoRead = reportNoRead;
         }
 
         public int getProdjectCount() {
@@ -165,20 +227,28 @@ public class HomeBean {
             this.isIslandNumber = isIslandNumber;
         }
 
+        public int getIsIslandNoRead() {
+            return isIslandNoRead;
+        }
+
+        public void setIsIslandNoRead(int isIslandNoRead) {
+            this.isIslandNoRead = isIslandNoRead;
+        }
+
+        public int getEarnestMoneyNoRead() {
+            return earnestMoneyNoRead;
+        }
+
+        public void setEarnestMoneyNoRead(int earnestMoneyNoRead) {
+            this.earnestMoneyNoRead = earnestMoneyNoRead;
+        }
+
         public int getAuditNumber() {
             return auditNumber;
         }
 
         public void setAuditNumber(int auditNumber) {
             this.auditNumber = auditNumber;
-        }
-
-        public int getLandingNumber() {
-            return landingNumber;
-        }
-
-        public void setLandingNumber(int landingNumber) {
-            this.landingNumber = landingNumber;
         }
     }
 }
